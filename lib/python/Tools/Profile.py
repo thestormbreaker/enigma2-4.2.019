9ff5a311bb5c28bab941be5d6f371e925ee15322
thestormbreaker <33662974+thestormbreaker@users.noreply.github.com>
2018-11-29 10:52:32 +0100

    Update Profile.py

--- lib/python/Tools/Profile.py
+++ lib/python/Tools/Profile.py
@@ -3,9 +3,7 @@ from boxbranding import getBoxType, getMachineBuild
 import time
 from Directories import resolveFilename, SCOPE_CONFIG
 
-boxtype = getBoxType()
-
-PERCENTAGE_START = 50
+PERCENTAGE_START = 0
 PERCENTAGE_END = 100
 
 profile_start = time.time()
