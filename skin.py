2bc893b8dd82f7f57d96f9f2b63864aad5c25430
thestormbreaker <33662974+thestormbreaker@users.noreply.github.com>
2018-12-06 08:39:05 +0100

    Update skin.py

--- skin.py
+++ skin.py
@@ -288,11 +288,9 @@ def collectAttributes(skinAttributes, node, context, skin_path_prefix=None, igno
 
 def morphRcImagePath(value):
 	if rc_model.rcIsDefault() is False:
-		if value == '/usr/share/enigma2/skin_default/rc.png' or value == '/usr/share/enigma2/skin_default/rcold.png':
+		if ('rc.png' or 'oldrc.png') in value:
 			value = rc_model.getRcLocation() + 'rc.png'
-		elif value == '/usr/share/enigma2/skin_default/rc0.png' or value == '/usr/share/enigma2/skin_default/rc1.png' or value == '/usr/share/enigma2/skin_default/rc2.png':
-			value = rc_model.getRcLocation() + 'rc.png'
-return value
+	return value
 
 def loadPixmap(path, desktop):
 	option = path.find("#")
