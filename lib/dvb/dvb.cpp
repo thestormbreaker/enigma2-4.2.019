d8763fdc2a8b09ec4f78fa3b2d51ecee256b3c6d
thestormbreaker <33662974+thestormbreaker@users.noreply.github.com>
2018-12-11 09:24:09 +0100

    Update dvb.cpp

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -137,7 +137,7 @@ eDVBResourceManager::eDVBResourceManager()
 	}		
 		
 	eDebug("[eDVBResourceManager] found %zd adapter, %zd frontends(%zd sim) and %zd demux,boxtype %d",
-		m_adapter.size(), m_frontend.size(), m_simulate_frontend.size(), m_boxtype), m_demux.size());
+		m_adapter.size(), m_frontend.size(), m_simulate_frontend.size(), m_demux.size(), m_boxtype);
 
 	m_fbcmng = new eFBCTunerManager(instance);
 
